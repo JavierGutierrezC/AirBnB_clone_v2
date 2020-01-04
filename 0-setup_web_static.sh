0421caafd2b837212be4520310c932c008af2abb
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-04 01:15:50 +0000

    [Fix]26i

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,6 +20,6 @@ echo "<html>
 sudo mkdir /data/web_static/shared/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '23i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '26i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
