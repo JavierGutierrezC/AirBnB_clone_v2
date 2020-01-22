4d79a51f036da463fef550ba96fa85ce5d0141f1
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-22 20:22:42 +0000

    [Fix]changed order of close

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,6 +7,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def SQL_close(self):
+    ''' remove the current SQLAlchemy Session '''
+    storage.close()
+
+
 @app.route("/states_list", strict_slashes=False)
 def states_airbnb():
     objects = storage.all("State")
@@ -19,11 +25,6 @@ def states_airbnb():
     # print(new_dict)
     return render_template('7-states_list.html', new_dict2=new_dict)
 
-
-@app.teardown_appcontext
-def SQL_close(self):
-    ''' remove the current SQLAlchemy Session '''
-    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
